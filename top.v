4b6db7af8b370d42e65be39cd571587843df1b24
HolmanCanyon <canyonholman@u.boisestate.edu>
2026-03-10 09:19:23 -0600

    Update top.v

--- top.v
+++ top.v
@@ -7,3 +7,14 @@ module ternary_2_mux_2_bits(
     assign Y = (Sel ? B : A);
 
 endmodule
+
+module equivalence_check(
+    input [1:0] select,
+    output is_zero, is_one, is_two, is_three
+);
+    // Option one:
+    assign is_zero  = ~select[0] && ~select[1]; // 0b00
+    assign is_one   =  select[0] && ~select[1]; // 0b01
+    assign is_two   = ~select[0] &&  select[1]; // 0b10
+    assign is_three =  select[0] &&  select[1]; // 0b11
+endmodule
