479fa0623f252190c9657d4309cefd98707c9648
HolmanCanyon <canyonholman@u.boisestate.edu>
2026-03-10 09:41:46 -0600

    Update top.v

--- top.v
+++ top.v
@@ -22,17 +22,5 @@ module demux(
 
 endmodule
 
-module equivalence_check(
-    input [1:0] select,
-    output is_zero, is_one, is_two, is_three
-);
-    // Option one:
-    assign is_zero  = ~select[0] && ~select[1]; // 0b00
-    assign is_one   =  select[0] && ~select[1]; // 0b01
-    assign is_two   = ~select[0] &&  select[1]; // 0b10
-    assign is_three =  select[0] &&  select[1]; // 0b11
-endmodule
-
-
 
 
